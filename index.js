e55616856e8538869fd4e5a5e1afc35a2e3df556
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-12-10 11:56:07 +1100

    Introduced tabnavigator for the sample app

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+import React from "react";
+import { Text, View } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+
+class ScreenOne extends React.Component<{}, {}> {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <Text>This is a simple example</Text>
+            </View>
+        );
+    }
+}
+
+class ScreenTwo extends React.Component<{}, {}> {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <Text>This is the other screen</Text>
+            </View>
+        );
+    }
+}
+
+const TabNavigator = createBottomTabNavigator({ Home: ScreenOne, Next: ScreenTwo });
+const AppContainer = createAppContainer(TabNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);
